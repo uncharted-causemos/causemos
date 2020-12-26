90490692028eac6ec60be95cc9830a8f2b3a9bc7
Daniel Chang <mwdchang@gmail.com>
2020-12-25 23:01:35 -0500

    project service stub

---
+++ client/src/services/project-service.js
@@ -0,0 +1,43 @@
+import API from '@/api/api';
+import { startPolling } from '@/api/poller';
+
+const KB_LIMIT = 200;
+const PROJECT_LIMIT = 500;
+
+const getKBs = async () => {
+  const result = await API.get('kbs', { params: { size: KB_LIMIT } });
+  return result.data;
+};
+
+const getProjects = async () => {
+  const result = API.get('projects', { params: { size: PROJECT_LIMIT } });
+  return result.data;
+};
+
+// Create new project based on KB specified by baseId
+const createProject = async (baseId, projectName, projectDescrption) => {
+  const result = await API.post('projects', {
+    baseId: baseId,
+    projectName: projectName,
+    description: projectDescrption
+  });
+  const id = result.data.index;
+
+  // Copying KB data is not instataneous, need to poll for ready state
+  const taskFn = async () => {
+    const status = await API.get(`projects/${id}/health`);
+    return status.data.indexStatus === 'green' ? [true, status] : [false, null];
+  };
+  const pollerConfig = {
+    interval: 2000,
+    threshold: 10
+  };
+  await startPolling(taskFn, pollerConfig);
+};
+
+export default {
+  getKBs,
+  getProjects,
+  createProject
+};
+
