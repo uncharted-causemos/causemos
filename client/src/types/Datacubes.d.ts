a59be922be12ccb3aabff33b454ba0cc9fc7a23f
Jaehwan Ryu <jryu@uncharted.software>
2021-03-30 11:11:13 -0400

    Added datacube type

--- client/src/types/Datacubes.d.ts
+++ client/src/types/Datacubes.d.ts
@@ -1,7 +1,24 @@
 export interface Datacube {
   id: string;
+  type: string;
   model: string;
-  output_units: string;
+  model_id: string;
+  category: string[];
+  model_description: string;
+  label: string;
+  maintainer: string;
   source: string;
+  output_name: string;
   output_description: string;
+  output_units: string;
+  output_units_description: string;
+  parameters: string[];
+  parameter_descriptions: string[];
+  concepts: { name: string; score: number }[];
+  country: string[];
+  admin1: string[];
+  admin2: string[];
+  period: { gte: string; lte: string}[];
+  variable?: string; // indicator variable
+  _search_score?: number;
 }
