f87b3466ad847edba70598dc2d317879e7e792da
Ben Codrington <bcodrington@uncharted.software>
2021-04-16 11:57:18 -0400

    Add breakdown pane to comp analysis experiment

--- client/src/components/drilldown-panel/aggregation-checklist-item.vue
+++ client/src/components/drilldown-panel/aggregation-checklist-item.vue
@@ -164,12 +164,12 @@ span.faded {
 
 .histogram-bar {
   position: absolute;
-  bottom: 100%;
+  top: 100%;
   left: 0;
   width: 100%;
   height: 4px;
   // TODO: add support for a function parameter to determine colour
-  background: blue;
+  background: #8767C8;
 
   &.faded {
     background: #B3B4B5;
--- client/src/views/CompAnalysisExperiment.vue
+++ client/src/views/CompAnalysisExperiment.vue
@@ -3,19 +3,79 @@
     <button class="search-button btn btn-primary btn-call-for-action" disabled>
       Search datacubes
     </button>
+    <main>
     <!-- TODO: whether a card is actually expanded or not will
     be dynamic later -->
+    <!-- TODO: we'll need to store the admin level data for each card
+    in this component and pass it down -->
     <datacube-card :class="{ 'datacube-expanded': true }" />
+    <drilldown-panel
+        class="drilldown"
+        :is-open="activeDrilldownTab !== null"
+        :tabs="drilldownTabs"
+        :active-tab-id="activeDrilldownTab"
+      >
+        <template #content>
+          <breakdown-pane
+            v-if="activeDrilldownTab ==='breakdown'"
+            :selected-admin-level="selectedAdminLevel"
+            :admin-level-data="adminLevelData"
+            :available-admin-levels="availableAdminLevels"
+            :type-breakdown-data="typeBreakdownData"
+            @set-selected-admin-level="setSelectedAdminLevel"
+          />
+        </template>
+    </drilldown-panel>
+    </main>
   </div>
 </template>
 
 <script lang="ts">
 import DatacubeCard from '@/components/data/datacube-card.vue';
-import { defineComponent } from 'vue';
+import DrilldownPanel from '@/components/drilldown-panel.vue';
+import ADMIN_LEVEL_DATA, { AGGREGATED_PRODUCT_TYPE_DATA } from '@/assets/admin-stats.js';
+import { defineComponent, ref } from 'vue';
+import BreakdownPane from '@/components/drilldown-panel/breakdown-pane.vue';
+
+const DRILLDOWN_TABS = [
+  {
+    name: 'Breakdown',
+    id: 'breakdown',
+    // TODO: our version of FA doesn't include fa-chart
+    icon: 'fa-question'
+  }
+];
 
 export default defineComponent({
   name: 'CompAnalysisExperiment',
-  components: { DatacubeCard }
+  components: { DatacubeCard, DrilldownPanel, BreakdownPane },
+  setup() {
+    const selectedAdminLevel = ref(1);
+    function setSelectedAdminLevel(newValue: number) {
+      selectedAdminLevel.value = newValue;
+    }
+    const availableAdminLevels = [
+      'Country',
+      'L1 admin region',
+      'L2 admin region',
+      'L3 admin region',
+      'L4 admin region',
+      'L5 admin region'
+    ].slice(0, ADMIN_LEVEL_DATA.maxDepth);
+
+    const typeBreakdownData = [
+      AGGREGATED_PRODUCT_TYPE_DATA
+    ];
+    return {
+      drilldownTabs: DRILLDOWN_TABS,
+      activeDrilldownTab: 'breakdown',
+      adminLevelData: ADMIN_LEVEL_DATA.data,
+      selectedAdminLevel,
+      setSelectedAdminLevel,
+      availableAdminLevels,
+      typeBreakdownData
+    };
+  }
 });
 </script>
 
@@ -23,17 +83,26 @@ export default defineComponent({
 @import '~styles/variables';
 .comp-analysis-experiment-container {
   height: $content-full-height;
-  padding: 10px;
   display: flex;
   flex-direction: column;
+  overflow: hidden;
+}
+
+main {
+  flex: 1;
+  display: flex;
+  min-height: 0;
 }
 
 .datacube-expanded {
+  min-width: 0;
   flex: 1;
+  margin: 10px;
+  margin-top: 0;
 }
 
 .search-button {
   align-self: center;
-  margin-bottom: 10px;
+  margin: 10px 0;
 }
 </style>
