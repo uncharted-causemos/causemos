3379da487cf77661dd79b45d8857d63c2529eebd
Daniel Chang <dchang@uncharted.software>
2021-04-20 09:51:53 -0400

    update not-found to typescript

--- client/src/views/NotFound.vue
+++ client/src/views/NotFound.vue
@@ -1,17 +1,23 @@
 <template>
-  <div class="row">
-    <div class="col-md-1" />
-    <div class="col-md-10">
-      <h2><i class="fa fa-exclamation-triangle" /> Page Not Found ... <a href="#/">Go Home</a></h2>
-    </div>
-    <div class="col-md-1" />
+  <div class="not-found">
+    <h2><i class="fa fa-fw fa-exclamation-triangle" /> Page Not Found ... <a href="#/">Go Home</a></h2>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'NotFound',
-  mounted() {
-  }
-};
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'NotFound'
+});
 </script>
+
+<style lang="scss" scoped>
+.not-found {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin: 40px 0px;
+}
+</style>
