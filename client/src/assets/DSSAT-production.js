e19086772ebd2246803e797ce55bce51035dc1f8
bcodrington <bcodrington@uncharted.software>
2021-04-21 18:06:13 -0400

    Remove duplicate scenario's ID from list

--- client/src/assets/DSSAT-production.js
+++ client/src/assets/DSSAT-production.js
@@ -3,7 +3,6 @@ export default {
   scenarioIds: [
     '967a0a69-552f-4861-ad7f-0c1bd8bab856',
     '057d28d5-a7ed-472b-ae37-ba16571944ea',
-    '25e0971b-c229-4c9a-a0f9-c121fce51309',
     '2ff53645-d481-4ff7-a067-e13f392f30a4',
     '2d80c9f0-1e44-4a6c-91fe-2ebb26e39dea'
   ]
