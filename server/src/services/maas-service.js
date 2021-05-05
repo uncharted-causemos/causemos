f223ce1c25c316ff7e0167bf3d814a14f7ed2777
Maciej Kozlowski <mkozlowski@uncharted.software>
2021-05-05 14:53:20 -0400

    linter!!!

--- server/src/services/maas-service.js
+++ server/src/services/maas-service.js
@@ -1,6 +1,3 @@
-const requestAsPromise = rootRequire('/util/request-as-promise');
-const Logger = rootRequire('/config/logger');
-
 const { Adapter, RESOURCE, SEARCH_LIMIT } = rootRequire('/adapters/es/adapter');
 
 /**
@@ -9,7 +6,6 @@ const { Adapter, RESOURCE, SEARCH_LIMIT } = rootRequire('/adapters/es/adapter');
  * @param{string} modelId - model id
  */
 const getAllModelRuns = async(modelId) => {
-  console.log('Getting all model runs for ' + modelId);
   const connection = Adapter.get(RESOURCE.MODEL_RUNS);
   const results = await connection.find([{ field: 'model_id', value: modelId }], { size: SEARCH_LIMIT });
   return results;
