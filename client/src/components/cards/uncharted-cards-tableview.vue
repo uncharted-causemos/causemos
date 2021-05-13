9f17ff58ac3cae95a22367baa1c3c320ce5b1e95
Jason Baker <jbaker@uncharted.software>
2021-05-13 13:47:27 -0400

    Table view for cards.

---
+++ client/src/components/cards/uncharted-cards-tableview.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>TABLE VIEW!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UnchartedCardsTableview',
+  props: {
+    data: {
+      type: Array,
+      default: () => []
+    },
+    config: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+@import '~styles/variables';
+
+</style>
