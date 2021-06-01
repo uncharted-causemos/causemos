392d1f253fbf8657b8e28a68b86f1b804afc10e7
Ahmed Mostafa <amostafa@uncharted.software>
2021-06-01 10:36:36 -0400

    Apply 1 suggestion(s) to 1 file(s)

--- client/src/views/ModelPublishingExperiment.vue
+++ client/src/views/ModelPublishingExperiment.vue
@@ -271,7 +271,7 @@ export default defineComponent({
       }
     }
 
-    const selectedTimestamp = ref(null) as Ref<number | null>;
+    const selectedTimestamp = ref<number | null>(null);
 
     const openPublishAccordion = ref(false);
 
