855ea75f4ec1c319b44e71438a434f12580c5b13
karlolsonuc <51097558+karlolsonuc@users.noreply.github.com>
2021-05-31 17:11:04 -0700

    #184 typescripting the new service

--- client/src/services/new-datacube-service.ts
+++ client/src/services/new-datacube-service.ts
@@ -1,10 +1,11 @@
 import API from '@/api/api';
+import { Filters } from '@/types/Filters';
 
 /**
  * Get datacubes
  * @param {Filters} filters
  */
-export const getDatacubes = async (filters) => {
+export const getDatacubes = async (filters: Filters) => {
   const { data } = await API.get(`maas/new-datacubes?filters=${JSON.stringify(filters)}`);
   return data;
 };
