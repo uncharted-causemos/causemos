6d71caec54c0b4f9e4a60a7ef072403085d7d20e
Daniel Chang <dchang@uncharted.software>
2021-06-22 17:13:27 -0400

    bring-your-own-document tracking plumbing

--- server/src/adapters/es/adapter.js
+++ server/src/adapters/es/adapter.js
@@ -19,7 +19,6 @@ const RESOURCE = Object.freeze({
   STATEMENT: 'statement',
   DOCUMENT: 'corpus',
   DOCUMENT_CONTEXT: 'document-context',
-  PROJECT_EXTENSION: 'project-extension',
 
   // Quantitative data
   NODE_PARAMETER: 'node-parameter',
@@ -35,7 +34,12 @@ const RESOURCE = Object.freeze({
   INSIGHT: 'insight',
   ONTOLOGY: 'ontology',
   SESSION_LOG: 'session-log',
-  INDICATOR_METADATA: 'indicator-metadata'
+  INDICATOR_METADATA: 'indicator-metadata',
+
+
+  // Tracking
+  ASSEMBLY_REQUEST: 'assembly-request',
+  PROJECT_EXTENSION: 'project-extension'
 });
 
 class Adapter {
