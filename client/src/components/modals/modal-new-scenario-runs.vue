7af585409c7c9412b703f1c4c523cdea8ad5f2dd
Ahmed <amostafa@uncharted.software>
2021-06-23 15:06:21 -0400

    Add support for executing multiple model runs

--- client/src/components/modals/modal-new-scenario-runs.vue
+++ client/src/components/modals/modal-new-scenario-runs.vue
@@ -98,36 +98,39 @@ export default defineComponent({
     this.potentialRuns = _.cloneDeep(this.potentialScenarios);
   },
   methods: {
-    startExecution() {
+    async startExecution() {
       // FIXME: cast to 'any' since typescript cannot see mixins yet!
       (this as any).toaster('New runs requested\nPlease check back later!');
 
-      // FIXME: only submitting ONE sceanrio is supported at this time
-      const firstScenario = this.potentialRuns[0];
-      const paramArray: any[] = [];
+      const modelId = this.metadata.id;
       const outputs = this.metadata.validatedOutputs ? this.metadata.validatedOutputs : this.metadata.outputs;
-      Object.keys(firstScenario).forEach(key => {
-        // exclude output variable values since they will be undefined for potential runs
-        if (key !== outputs[this.currentOutputIndex].name) {
-          paramArray.push({
-            name: key,
-            value: firstScenario[key]
-          });
-        }
-      });
       const drilldownParams = this.metadata.parameters.filter(d => d.is_drilldown);
-      drilldownParams.forEach(p => {
-        paramArray.push({
-          name: p.name,
-          value: p.default
+
+      const promises = this.potentialRuns.map(async (modelRun) => {
+        const paramArray: any[] = [];
+        Object.keys(modelRun).forEach(key => {
+          // exclude output variable values since they will be undefined for potential runs
+          if (key !== outputs[this.currentOutputIndex].name) {
+            paramArray.push({
+              name: key,
+              value: modelRun[key]
+            });
+          }
+        });
+        drilldownParams.forEach(p => {
+          paramArray.push({
+            name: p.name,
+            value: p.default
+          });
+        });
+        return API.post('maas/model-runs', {
+          model_id: modelId,
+          model_name: this.metadata?.name,
+          parameters: paramArray
         });
       });
-      const modelId = this.metadata.id;
-      API.post('maas/model-runs', {
-        model_id: modelId,
-        model_name: this.metadata?.name,
-        parameters: paramArray
-      });
+      // wait until all promises are resolved
+      await Promise.all(promises);
 
       this.close(false);
     },
