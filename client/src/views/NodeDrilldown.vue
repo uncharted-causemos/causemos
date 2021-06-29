9bdd632349c07b019338de15fffc905a8e350de1
bcodrington <bcodrington@uncharted.software>
2021-06-29 16:52:01 -0400

    Add routing to new page on node double click

---
+++ client/src/views/NodeDrilldown.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="node-drilldown-container">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'NodeDrilldown',
+  setup() {
+    // TODO: get CAG and selected node from route
+    // TODO: fetch top drivers and top impacts (as well as edge colours/weights)
+    //  CLARIFICATION REQUIRED:
+    //    is this taken from the sensitivity analysis?
+    //    do we want a maximum number of drivers/impacts? or add a scrollbar?
+    // TODO: fetch historical/projection data for each driver/impact and selected node.
+    return {
+
+    };
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
