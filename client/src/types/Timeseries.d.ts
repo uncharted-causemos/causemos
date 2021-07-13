793a7ecd37ba042dde1bc8452d5909486f5ddb65
bcodrington <bcodrington@uncharted.software>
2021-07-13 12:23:42 -0400

    typo

--- client/src/types/Timeseries.d.ts
+++ client/src/types/Timeseries.d.ts
@@ -12,7 +12,7 @@ export interface Timeseries {
 /**
  * Contains all of the information to distinguish between timeseries points at a given timestamp.
  * * isTimestampInTimeseries is included to indicate occurrences where one of the timeseries
- * doesn't have a oint at the selected timestamp.
+ * doesn't have a point at the selected timestamp.
  */
 export interface TimeseriesPointSelection {
   timeseriesId: string;
