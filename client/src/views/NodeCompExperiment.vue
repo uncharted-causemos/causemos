bb0eee8f8b02bdc4085ab124ce038869ed3f2498
bcodrington <bcodrington@uncharted.software>
2021-07-28 10:18:35 -0400

    Replace dummy data - disable confirm valid when it's not clear which data would be returned

--- client/src/views/NodeCompExperiment.vue
+++ client/src/views/NodeCompExperiment.vue
@@ -5,15 +5,19 @@
       :nav-back-label="navBackLabel"
       @close="onBack"
     >
-      <button
-        v-tooltip.top-center="selectLabel"
-        type="button"
-        class="btn btn-primary btn-call-for-action"
-        @click="onSelection"
-      >
-        <i class="fa fa-fw fa-plus-circle" />
-        {{selectLabel}}
-      </button>
+      <div class="header-content">
+        <button
+          v-tooltip.top-center="selectLabel"
+          :disabled="stepsBeforeCanConfirm.length > 0"
+          type="button"
+          class="btn btn-primary btn-call-for-action"
+          @click="onSelection"
+        >
+          <i class="fa fa-fw fa-plus-circle" />
+          {{selectLabel}}
+        </button>
+        <span v-if="stepsBeforeCanConfirm.length > 0">{{ stepsBeforeCanConfirm[0] }}</span>
+      </div>
     </full-screen-modal-header>
     <main>
       <datacube-card
@@ -319,6 +323,22 @@ export default defineComponent({
       selectedTimeseriesPoints
     );
 
+    const stepsBeforeCanConfirm = computed(() => {
+      const steps = [];
+      if (metadata.value === null) {
+        steps.push('Loading...');
+      }
+      if (selectedScenarioIds.value.length < 1) {
+        steps.push('Please select a scenario.');
+      } else if (selectedScenarioIds.value.length > 1) {
+        steps.push('Please select exactly one scenario.');
+      }
+      if (breakdownOption.value !== null) {
+        steps.push('Please set "split by" to "none".');
+      }
+      return steps;
+    });
+
     const selectLabel = 'Quantify Node';
     const navBackLabel = 'Select A Different Datacube';
     return {
@@ -365,7 +385,8 @@ export default defineComponent({
       selectedTimeseriesPoints,
       currentCAG,
       nodeId,
-      project
+      project,
+      stepsBeforeCanConfirm
     };
   },
   unmounted(): void {
@@ -404,32 +425,16 @@ export default defineComponent({
       });
     },
 
-    onSelection () {
-      // FIXME: to do get actual timeseries data here
-      const timeseries = [
-        {
-          timestamp: 1527811200000, value: 10.5
-        },
-        {
-          timestamp: 1530403200000, value: 13.5
-        },
-        {
-          timestamp: 1533081600000, value: 24.5
-        },
-        {
-          timestamp: 1535760000000, value: 15.2
-        },
-        {
-          timestamp: 1538352000000, value: 24.9
-        },
-        {
-          timestamp: 1541030400000, value: 30.2
-        },
-        {
-          timestamp: 1543622400000, value: 11.2
-        }
-      ];
-
+    async onSelection() {
+      if (this.metadata === null) {
+        console.error('Confirm should not be clickable until metadata is loaded.');
+        return;
+      }
+      if (this.visibleTimeseriesData.length !== 1) {
+        console.error('There should be exactly one timeseries visible.', this.visibleTimeseriesData);
+        return;
+      }
+      const timeseries = this.visibleTimeseriesData[0].points;
       const { max, min } = projectionValueRange(timeseries.map(d => d.value));
       const nodeParameters = {
         id: this.selectedNode.id,
@@ -437,24 +442,23 @@ export default defineComponent({
         label: this.selectedNode.label,
         model_id: this.selectedNode.model_id,
         parameter: {
-          id: this.outputSpecs[this.currentOutputIndex].id,
-          name: this.mainModelOutput?.display_name,
-          unit: this.mainModelOutput?.unit,
-          // to do respect selections made by users in the experiment view
+          id: this.metadata.id,
+          name: this.metadata.name,
+          unit: this.unit,
+          // TODO: respect regional selections made by users in the experiment view
           country: '',
           admin1: '',
           admin2: '',
           admin3: '',
-          geospatial_aggregation: this.outputSpecs[this.currentOutputIndex].spatialAggregation,
-          temporal_aggregation: this.outputSpecs[this.currentOutputIndex].temporalAggregation,
-          temporal_resolution: this.outputSpecs[this.currentOutputIndex].temporalResolution,
+          geospatial_aggregation: this.selectedSpatialAggregation,
+          temporal_aggregation: this.selectedTemporalAggregation,
+          temporal_resolution: this.selectedTemporalResolution,
           period: 12,
           max: max,
           min: min
         }
       };
-      modelService.updateNodeParameter(this.selectedNode.model_id, nodeParameters);
-
+      await modelService.updateNodeParameter(this.selectedNode.model_id, nodeParameters);
 
       this.$router.push({
         name: 'nodeDrilldown',
@@ -534,6 +538,10 @@ export default defineComponent({
   display: flex;
   flex-direction: column;
   overflow: hidden;
+}
+
+.header-content > *:not(:first-child) {
+  margin-left: 5px;
 }
 
 main {
