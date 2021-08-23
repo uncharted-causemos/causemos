59cc02890bda3614f0aeb12d4cd30170c7c31693
Jason Baker <jbaker@uncharted.software>
2021-08-23 11:30:52 -0400

    Ensure line clears after shift release.

--- client/src/charts/parallel-coordinates.ts
+++ client/src/charts/parallel-coordinates.ts
@@ -595,7 +595,7 @@ function renderParallelCoordinates(
       });
     if (brushesCount === 0) {
       // cancel any previous selection; turn every line into grey
-      if (event && !event.shiftKey && (!d || currentLineSelection.length === 1)) {
+      if (event && !event.shiftKey && (!d || currentLineSelection.length)) {
         currentLineSelection.length = 0;
         cancelPrevLineSelection(svgElement);
       }
