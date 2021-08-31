57df0bbef6688436a7b847e384682f5dd17cb776
Venkat Korapaty <vkorapaty@admins-MacBook-Pro.local>
2021-08-31 13:27:44 -0400

    made indicator search less generous

--- server/src/services/indicator-service.js
+++ server/src/services/indicator-service.js
@@ -150,9 +150,12 @@ const getOntologyCandidates = async (modelId, filteredNodeParameters) => {
     compositionalConcepts = [...new Set(compositionalConcepts)];
 
     const compositionalKeywords = [...new Set(compositionalConcepts.map(comp => {
-      const words = comp.split('/').slice(2);
+      const words = comp.split('/').slice(3);
       return words.map(word => word.split('_')).flat(1);
     }).flat(1))];
+
+    // to match at least the number of words in the original concepts
+    // const minimumConceptWords = concept.split('/').slice(3)[0].split('_').length;
 
     const searchPayload = {
       index: RESOURCE.DATA_DATACUBE,
@@ -178,13 +181,14 @@ const getOntologyCandidates = async (modelId, filteredNodeParameters) => {
                       terms: {
                         description: compositionalKeywords
                       }
-                    },
-                    {
-                      terms: {
-                        'outputs.description': compositionalKeywords
-                      }
                     }
+                    // {
+                    //   terms: {
+                    //     'outputs.description': compositionalKeywords
+                    //   }
+                    // }
                   ]
+                  // minimum_should_match: minimumConceptWords
                 }
               },
               {
