fcd7f7690f9e0d12346cc08513ec62070b8b6d71
karlolsonuc <51097558+karlolsonuc@users.noreply.github.com>
2021-09-01 18:25:45 -0700

    #453 refactor to reduce code duplication in edit and new insight layout
    (probably) part one of reducing code duplication in edit and new insight views such that the layout which mostly identical except autofill (as edit doesn't have an autofill) is all in one layout component reused by both new and edit insight modals. part two will probably be moving any duplicate data parsing code in computeds and methods of into a util that both edit and new insight components can use. This work should serve as a refactor model for other areas with duplicate layout but different data sources. Also cut various used items in new and edit.

--- client/src/components/insight-manager/new-insight-modal.vue
+++ client/src/components/insight-manager/new-insight-modal.vue
@@ -1,111 +1,26 @@
 <template>
-  <div class="new-insight-modal-container">
-    <full-screen-modal-header>
-      <h5>New Insight</h5>
-    </full-screen-modal-header>
-    <div class="pane-wrapper">
-      <div class="pane-row">
-        <div class="fields">
-          <div class="preview" v-if="imagePreview !== null">
-            <img :src="imagePreview">
-          </div>
-          <disclaimer
-            v-else
-            style="text-align: center; color: black"
-            :message="'No image preview!'"
-          />
-          <div class="form-group">
-            <form>
-              <label> Name* </label>
-              <input
-                v-model="name"
-                v-focus
-                type="text"
-                class="form-control"
-                placeholder="Untitled insight"
-                @keyup.enter.stop="saveInsight"
-              >
-              <div
-                v-if="hasError === true"
-                class="error-msg">
-                {{ errorMsg }}
-              </div>
-              <label>Description</label>
-              <textarea
-                rows="2"
-                v-model="description"
-                class="form-control" />
-            </form>
-          </div>
-          <div class="controls">
-            <button
-              type="button"
-              class="btn btn-light"
-              @click="closeInsightPanel"
-            >
-              Cancel
-            </button>
-            <button
-              class="btn btn-primary"
-              @click="autofillInsight"
-            >
-              Autofill
-            </button>
-            <button
-              type="button"
-              class="btn btn-primary"
-              :class="{ 'disabled': name.length === 0}"
-              @click="saveInsight"
-            >
-              Save
-            </button>
-          </div>
-        </div>
-
-        <drilldown-panel
-          class="metadata-drilldown-panel"
-          is-open
-          :tabs="drilldownTabs"
-          :activeTabId="drilldownTabs[0].id"
-          only-display-icons
-        >
-          <template #content>
-            <div>
-              <ul>
-                <!--
-                <li>
-                  <i :class="iconToDisplay" /> {{ viewName }}
-                </li>
-                -->
-                <li
-                  v-for="metadataAttr in metadataDetails"
-                  :key="metadataAttr.key">
-                  <b>{{metadataAttr.key}}</b> {{ metadataAttr.value }}
-                </li>
-              </ul>
-            </div>
-          </template>
-        </drilldown-panel>
-
-      </div>
-    </div>
-  </div>
+  <new-edit-modal-layout
+    v-model:name="name"
+    v-model:description="description"
+    :image-preview="imagePreview"
+    :metadata-details="metadataDetails"
+    title="New Insight"
+    @auto-fill="autofillInsight()"
+    @cancel="closeInsight()"
+    @save="saveInsight()"
+  />
 </template>
 
 <script>
 import html2canvas from 'html2canvas';
 import _ from 'lodash';
 import { mapGetters, mapActions } from 'vuex';
-
-import DrilldownPanel from '@/components/drilldown-panel';
-import FullScreenModalHeader from '@/components/widgets/full-screen-modal-header';
+import NewEditModalLayout from '@/components/insight-manager/new-edit-modal-layout';
 import FilterValueFormatter from '@/formatters/filter-value-formatter';
 import FilterKeyFormatter from '@/formatters/filter-key-formatter';
 import modelService from '@/services/model-service';
-import { VIEWS_LIST } from '@/utils/views-util';
 import { INSIGHTS } from '@/utils/messages-util';
 import { ProjectType } from '@/types/Enums';
-import Disclaimer from '@/components/widgets/disclaimer';
 import { addInsight } from '@/services/insight-service';
 
 
@@ -122,9 +37,7 @@ const METDATA_DRILLDOWN_TABS = [
 export default {
   name: 'NewInsightModal',
   components: {
-    DrilldownPanel,
-    FullScreenModalHeader,
-    Disclaimer
+    NewEditModalLayout
   },
   data: () => ({
     description: '',
@@ -153,22 +66,8 @@ export default {
       project: 'app/project',
 
       filters: 'dataSearch/filters',
-      ontologyConcepts: 'dataSearch/ontologyConcepts',
-
-      view: 'query/view'
+      ontologyConcepts: 'dataSearch/ontologyConcepts'
     }),
-    iconToDisplay() {
-      const view = VIEWS_LIST.find(item => item.id === this.currentView);
-      return _.isNil(view)
-        ? ''
-        : view.icon;
-    },
-    viewName() {
-      const view = VIEWS_LIST.find(item => item.id === this.currentView);
-      return _.isNil(view)
-        ? ''
-        : view.name;
-    },
     formattedFilterString() {
       const filterString = this.filters?.clauses?.reduce((a, c) => {
         return a + `${a.length > 0 ? ' AND ' : ''} ` +
@@ -405,89 +304,3 @@ export default {
   }
 };
 </script>
-
-<style lang="scss" scoped>
-@import "~styles/variables";
-
-.new-insight-modal-container {
-  display: flex;
-  flex-direction: column;
-  flex-wrap: nowrap;
-  justify-content: center;
-  align-content: stretch;
-  align-items: stretch;
-  height: 100vh;
-  overflow: hidden;
-
-  .pane-wrapper {
-    flex: 1 1 auto;
-    display: flex;
-    flex-direction: column;
-    overflow-x: hidden;
-    overflow-y: hidden;
-    padding: 1em 0 0;
-    .pane-row {
-      flex: 1 1 auto;
-      display: flex;
-      flex-direction: row;
-      height: 100%;
-      .fields {
-        flex: 1 1 auto;
-        display: flex;
-        flex-direction: column;
-        overflow: hidden;
-        padding-left: 1rem;
-        padding-right: 1rem;
-        height: 100%;
-        .preview {
-          flex: 1 1 auto;
-          margin: 0 0 1rem;
-          overflow: hidden;
-          align-self: center;
-          img {
-            max-height: 100%;
-            max-width: 100%;
-          }
-        }
-        .form-group {
-          flex: 0 0 auto;
-          margin-bottom: 3px;
-          form {
-            display: flex;
-            flex-direction: column;
-            width: 100%;
-            textarea {
-              flex: 1 1 auto;
-              resize: none;
-              outline: none;
-              box-sizing: border-box;
-            }
-          }
-        }
-        .controls {
-          flex: 0 0 auto;
-          display: flex;
-          justify-content: flex-end;
-          padding: 1rem;
-          button {
-            margin-left: 1rem;
-          }
-        }
-      }
-    }
-  }
-
-
-}
-
-.error-msg {
-  color: $negative;
-}
-
-h6 {
-  @include header-secondary;
-  font-size: $font-size-medium;
-}
-
-</style>
-
