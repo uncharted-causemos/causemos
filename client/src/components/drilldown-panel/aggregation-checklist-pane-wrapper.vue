c09141c1de52e1494c58a2ead1834e729e1b94e2
Daniel Bruce <dbruce@uncharted.software>
2021-09-02 09:48:48 -0400

    aggregation checklist pane wrapper

---
+++ client/src/components/drilldown-panel/aggregation-checklist-pane-wrapper.vue
@@ -0,0 +1,31 @@
+<template>
+  <aggregation-checklist-pane
+    v-bind="$attrs"
+    :selected-item-ids="selectedItemIds"
+    :should-show-deselected-bars="shouldShowDeselectedBars || selectedItemIds.length === 0"
+  />
+</template>
+
+<script lang="ts">
+
+import AggregationChecklistPane from '@/components/drilldown-panel/aggregation-checklist-pane.vue';
+import { defineComponent, PropType } from '@vue/runtime-core';
+
+export default defineComponent({
+  name: 'AggregationChecklistPaneWrapper',
+  components: {
+    AggregationChecklistPane
+  },
+  props: {
+    shouldShowDeselectedBars: {
+      type: Boolean,
+      required: true
+    },
+    selectedItemIds: {
+      type: Object as PropType<string[]>,
+      default: []
+    }
+  }
+});
+
+</script>
