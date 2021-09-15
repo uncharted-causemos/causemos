77bb742370650173c14ccce39ffd11acc0e7aa44
Jaehwan Ryu <jryu@uncharted.software>
2021-09-15 16:39:58 -0400

    Update map legend

--- client/src/components/widgets/map-legend.vue
+++ client/src/components/widgets/map-legend.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="map-legend-container">
+  <div class="map-legend-container" v-bind:class="{ 'label-right': labelPosition.right, 'label-top': labelPosition.top }">
     <div
       v-for="d in ramp"
       :key="d.color"
@@ -8,6 +8,7 @@
       <span class="color-label">{{ d.label }}</span>
       <div class="color" v-bind:style="{backgroundColor: d.color }"></div>
     </div>
+    <span v-if="labelPosition.top" class="bottom-label">{{bottomLabel}}</span>
   </div>
 </template>
 <script lang="ts">
@@ -21,11 +22,23 @@ export default defineComponent({
     ramp: {
       type: Array as PropType<MapLegendColor[]>,
       default: []
+    },
+    labelPosition: {
+      type: Object as PropType<{ top: boolean; right: boolean }>,
+      default: { top: true, right: true }
+    },
+    // Optional label placed on the bottom of the legend (when label position is top)
+    bottomLabel: {
+      type: String,
+      default: ''
     }
   }
 });
 </script>
 <style lang="scss" scoped>
+.map-legend-container {
+  position: relative;
+}
 .color-row {
   margin: 2px;
   display: flex;
@@ -42,5 +55,32 @@ export default defineComponent({
   font-size: 10px;
   display: inline-flex;
   align-items: center;
+}
+.bottom-label {
+  font-size: 10px;
+  position: absolute;
+  right: 24px;
+  bottom: -5px;
+}
+.label-right {
+  .color-row {
+    flex-direction: row-reverse;
+  }
+  .color-label {
+    padding-right: 0px;
+    padding-left: 2px;
+  }
+  .bottom-label {
+    left: 20px;
+    bottom: -5px;
+  }
+}
+.label-top {
+  .color-label {
+    position: relative;
+    top: -8px;
+    flex-direction: column;
+    justify-content: space-between;
+  }
 }
 </style>
