6ccaa1edae000c31d976c636f31de230ae2e4f3e
Daniel Bruce <dbruce@uncharted.software>
2021-09-23 16:55:03 -0400

    check for breakdown option

--- client/src/services/composables/useQualifiers.ts
+++ client/src/services/composables/useQualifiers.ts
@@ -56,11 +56,11 @@ const convertResponsesToBreakdownData = (
         //  data list so that the user can select that qualifier option to see
         //  its timeseries.
         if (value !== null) {
-          if (_.isNull(breakdownOption)) {
-            potentiallyExistingOption.values[runId] = value;
-          } else {
+          if (breakdownOption) {
             // Key values here will change when we implement the missing functionality for these key values.
             potentiallyExistingOption.values[optionId] = value;
+          } else {
+            potentiallyExistingOption.values[runId] = value;
           }
         }
       });
