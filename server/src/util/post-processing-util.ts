371266bf518d995cb8e96a9e27e1a80348e445f0
Maciej Kozlowski <mkozlowski@uncharted.software>
2021-10-01 15:33:51 -0400

    Don't strip out negative timestamps

--- server/src/util/post-processing-util.ts
+++ server/src/util/post-processing-util.ts
@@ -15,13 +15,6 @@ const removeUnwantedData = (metadata) => {
 }
 
 const processFilteredData = (metadata) => {
-  // Apparently ES can't support negative timestamps
-  if (metadata.period && metadata.period.gte < 0) {
-    metadata.period.gte = 0;
-  }
-  if (metadata.period && metadata.period.lte < 0) {
-    metadata.period.lte = 0;
-  }
   metadata.family_name = metadata.family_name || metadata.name;
 };
 
