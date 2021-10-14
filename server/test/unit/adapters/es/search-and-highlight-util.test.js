c22eed4b77db772fb81e0df8985832a6f314c118
Daniel Bruce <dbruce@uncharted.software>
2021-10-14 10:15:32 -0400

    unit test rename

--- server/test/unit/adapters/es/search-and-highlight-util.test.js
+++ server/test/unit/adapters/es/search-and-highlight-util.test.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const { searchAndHighlight } = rootRequire('adapters/es/client');
 
 describe('search-and-highlight-util', function() {
-  describe('buildAggregation', function () {
-    it('handles basic terms aggregation', async() => {
+  describe('searchAndHighlight', function () {
+    it('ensures searchAndHighlight works', async() => {
       const results = await searchAndHighlight('gadm-name', 'ca', [], ['country', 'admin1']);
       console.log(results);
     });
