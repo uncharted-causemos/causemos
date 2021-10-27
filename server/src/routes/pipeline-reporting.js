8d14ea95014b6f8e463fc72eb2d68c6901258a35
Daniel Bruce <dbruce@uncharted.software>
2021-10-27 14:43:56 -0400

    pipeline succeeded endpoint

--- server/src/routes/pipeline-reporting.js
+++ server/src/routes/pipeline-reporting.js
@@ -11,4 +11,12 @@ router.put('/processing-failed', asyncHandler(async (req, res) => {
   await respondUsingCode(req, res, pipelineReportingService.setProcessingFailed);
 }));
 
+/**
+ * Processing failed endpoint sets the status of the relevant ES documents to `PROCESSING FAILED`.
+ */
+router.put('/processing-succeeded', asyncHandler(async (req, res) => {
+  const { respondUsingCode } = rootRequire('/util/model-run-util.ts');
+  await respondUsingCode(req, res, pipelineReportingService.setProcessingSucceeded);
+}));
+
 module.exports = router;
--- server/src/services/pipeline-reporting-service.js
+++ server/src/services/pipeline-reporting-service.js
@@ -4,6 +4,40 @@ const datacubeService = rootRequire('/services/datacube-service');
 /**
  * Update a set of documents so that their status is now `PROCESSING_FAILED`
  */
+const setProcessingSucceeded = async(metadata) => {
+  const docIds = metadata.doc_ids;
+  const isIndicator = metadata.is_indicator;
+  const newStatus = 'READY';
+  if (isIndicator) {
+    const metadataDelta = docIds.map(docId => {
+      return {
+        id: docId,
+        status: newStatus
+      };
+    });
+    await datacubeService.updateDatacube(metadataDelta);
+    return { result: { message: 'Datacube updated' }, code: 200 };
+  } else {
+    const modelRun = docIds.map(docId => {
+      return {
+        id: docId,
+        status: newStatus,
+        runtimes: {
+          post_processing: {
+            end_time: metadata.end_time,
+            start_time: metadata.start_time
+          }
+        }
+      };
+    });
+    await maasService.updateModelRun(modelRun);
+    return { result: { message: 'Model run updated' }, code: 200 };
+  }
+};
+
+/**
+ * Update a set of documents so that their status is now `PROCESSING_FAILED`
+ */
 const setProcessingFailed = async(metadata) => {
   const docIds = metadata.doc_ids;
   const isIndicator = metadata.is_indicator;
@@ -30,5 +64,6 @@ const setProcessingFailed = async(metadata) => {
 };
 
 module.exports = {
-  setProcessingFailed
+  setProcessingFailed,
+  setProcessingSucceeded
 };
