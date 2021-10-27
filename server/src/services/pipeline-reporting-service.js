b2f8970902911b730c75b861c6d3b999d44a971a
Daniel Bruce <dbruce@uncharted.software>
2021-10-27 16:36:46 -0400

    updated model runs

--- server/src/services/pipeline-reporting-service.js
+++ server/src/services/pipeline-reporting-service.js
@@ -2,7 +2,7 @@ const maasService = rootRequire('/services/external/maas-service');
 const datacubeService = rootRequire('/services/datacube-service');
 
 /**
- * Update a set of documents so that their status is now `PROCESSING_FAILED`
+ * Update a set of documents so that their status is now `READY`
  */
 const setProcessingSucceeded = async(metadata) => {
   const docIds = metadata.doc_ids;
@@ -63,7 +63,41 @@ const setProcessingFailed = async(metadata) => {
   }
 };
 
+/**
+ * Sets the runtimes.queued status for some documents
+ */
+const setRuntimeQueued = async(metadata) => {
+  const docIds = metadata.doc_ids;
+  const modelRun = docIds.map(docId => {
+    return {
+      id: docId,
+      runtimes: {
+        queued: {
+          end_time: metadata.end_time,
+          start_time: metadata.start_time
+        }
+      }
+    };
+  });
+  await maasService.updateModelRun(modelRun);
+  return { result: { message: 'Model run updated' }, code: 200 };
+};
+
+/**
+ * Sets the output aggregate values fields for a list of documents
+ */
+const setOutputAggregateValues = async(metadata) => {
+  const modelRun = {
+    id: metadata.run_id,
+    output_agg_values: metadata.summary
+  };
+  await maasService.updateModelRun(modelRun);
+  return { result: { message: 'Model run updated' }, code: 200 };
+};
+
 module.exports = {
   setProcessingFailed,
-  setProcessingSucceeded
+  setProcessingSucceeded,
+  setRuntimeQueued,
+  setOutputAggregateValues
 };
