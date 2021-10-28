9b0395b3a5a6dff017d899bd9defb8b346ee6910
bcodrington <bcodrington@uncharted.software>
2021-10-28 09:54:58 -0400

    Remove empty style tags

--- client/src/components/qualitative/qualitative-analysis-options-button.vue
+++ client/src/components/qualitative/qualitative-analysis-options-button.vue
@@ -101,5 +101,3 @@ export default defineComponent({
   }
 });
 </script>
-
-<style lang="scss" scoped></style>
--- client/src/components/qualitative/qualitative-comments-button.vue
+++ client/src/components/qualitative/qualitative-comments-button.vue
@@ -48,5 +48,3 @@ export default defineComponent({
   }
 });
 </script>
-
-<style lang="scss" scoped></style>
