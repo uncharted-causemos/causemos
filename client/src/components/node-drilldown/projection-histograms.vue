7f9ab596c1d6c54e6c802a7c8255a3c893b0c146
bcodrington <bcodrington@uncharted.software>
2021-11-11 17:03:07 -0500

    Missed changes last commit

---
+++ client/src/components/node-drilldown/projection-histograms.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="projection-histograms-container">
+  </div>
+</template>
+
+<script lang="ts">
+import { ScenarioProjection } from '@/types/CAG';
+import { TimeseriesPoint } from '@/types/Timeseries';
+import { defineComponent, PropType } from 'vue';
+export default defineComponent({
+  name: 'ProjectionHistograms',
+  props: {
+    comparisonBaselineId: {
+      type: String,
+      default: null
+    },
+    historicalTimeseries: {
+      type: Array as PropType<TimeseriesPoint[]>,
+      default: []
+    },
+    projections: {
+      type: Array as PropType<ScenarioProjection[]>,
+      default: []
+    }
+  },
+  setup() {
+    return {
+
+    };
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+</style>
