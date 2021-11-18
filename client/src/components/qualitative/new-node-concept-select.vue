dc30fb77020ed53624e8153ccee237c1a5fe4670
Andrew LaRiviere <alariviere@uncharted.software>
2021-11-18 09:58:28 -0800

    added correction for input box width

--- client/src/components/qualitative/new-node-concept-select.vue
+++ client/src/components/qualitative/new-node-concept-select.vue
@@ -117,9 +117,10 @@ export default {
   },
   mounted() {
     const inputBoundingBox = this.$refs.input.getBoundingClientRect();
-    const dropdownWidth = 0.45 * window.innerWidth;
+    const dropdownWidth = 0.45 * window.innerWidth; // convert vw to px
+    const inputWidth = 13 * parseFloat(getComputedStyle(document.documentElement).fontSize); // convert rem to px
     if (inputBoundingBox.left + dropdownWidth > window.innerWidth) {
-      this.dropdownLeftOffset = -dropdownWidth;
+      this.dropdownLeftOffset = -dropdownWidth + inputWidth;
     }
     console.log(this.dropdownLeftOffset);
   },
