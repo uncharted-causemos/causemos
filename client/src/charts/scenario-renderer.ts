1c6745db4f375ab55d24634e633364f9a79eca61
bcodrington <bcodrington@uncharted.software>
2021-11-18 18:17:56 -0500

    Choose history width based on adjustable constant

--- client/src/charts/scenario-renderer.ts
+++ client/src/charts/scenario-renderer.ts
@@ -1,11 +1,10 @@
-import _ from 'lodash';
 import * as d3 from 'd3';
 import moment from 'moment';
 
 import initialize from '@/charts/initialize';
 import { timeseriesLine, translate } from '@/utils/svg-util';
 import { chartValueFormatter } from '@/utils/string-util';
-import { D3GElementSelection, D3ScaleLinear, D3Selection } from '@/types/D3';
+import { D3GElementSelection, D3Selection } from '@/types/D3';
 import { Chart } from '@/types/Chart';
 import { NodeScenarioData } from '@/types/CAG';
 import { calculateGenericTicks } from '@/utils/timeseries-util';
@@ -17,6 +16,7 @@ const SPACE_BETWEEN_HISTOGRAMS = 2;
 const SPACE_BETWEEN_HISTOGRAM_BARS = 1;
 const HISTOGRAM_BACKGROUND_COLOR = HISTORY_BACKGROUND_COLOR;
 const HISTOGRAM_FOREGROUND_COLOR = HISTORY_LINE_COLOR;
+const HISTORY_WIDTH_PERCENTAGE = 0.5;
 
 export default function(
   selection: D3Selection,
@@ -46,65 +46,34 @@ function render(
     return;
   }
 
-  // Figure out the [start, end] ranges
-  let globalStart = Number.POSITIVE_INFINITY;
-  let globalEnd = Number.NEGATIVE_INFINITY;
-  nodeScenarioData.scenarios.forEach(scenario => {
-    const {
-      indicator_time_series_range,
-      projection_start,
-      num_steps
-    } = scenario.parameter;
-    const start = indicator_time_series_range.start;
-    const end = moment
-      .utc(projection_start)
-      .add(num_steps, 'months')
-      .valueOf();
-    if (start < globalStart) globalStart = start;
-    if (end > globalEnd) globalEnd = end;
-  });
+  // Calculate timestamp of the earliest historical time to display
+  const { projection_start } = selectedScenario.parameter;
+  // TODO: should this be a constant? based on time_scale? based on how many columns are hidden?
+  const historicalMonthsToDisplay = 5 * 12;
+  const historyStart = moment
+    .utc(projection_start)
+    .subtract(historicalMonthsToDisplay, 'months')
+    .valueOf();
+  const historyEnd = projection_start;
 
-  // Portion of time series relating to current scenario
-  const historyRange = selectedScenario.parameter.indicator_time_series_range;
+  // Filter out timeseries points that aren't within the range we're displaying
   const filteredTimeSeries = nodeScenarioData.indicator_time_series.filter(
-    d => d.timestamp >= historyRange.start && d.timestamp <= historyRange.end
+    d => d.timestamp >= historyStart && d.timestamp <= historyEnd
   );
 
-  // Chart params
+  // TODO: choose yExtent so that "now" value is centered
+  const yExtent = [nodeScenarioData.min, nodeScenarioData.max];
+
+  // Chart dimensions
   const height = chart.y2 - chart.y1;
   const width = chart.x2 - chart.x1;
-  let xExtent = null;
-  let yExtent = null;
-
-  xExtent = [globalStart, globalEnd];
-  if (_.isEmpty(filteredTimeSeries)) {
-    yExtent = [0, 0];
-  } else {
-    // Safe to assume yExtent is not [undefined, undefined] since
-    //  filteredTimeSeries is not empty
-    yExtent = d3.extent(filteredTimeSeries.map(d => d.value)) as [
-      number,
-      number
-    ];
-  }
-  if (yExtent[1] === yExtent[0] && yExtent[0] === 0) {
-    yExtent = [0, 1];
-  } else if (yExtent[1] === yExtent[0] && yExtent[0] !== 0) {
-    // if flat-line around non-zero, range becomes 0 and twice the value  (ex. 5.5 => [0,11]  -5.5 => [-11,0])
-    yExtent = [
-      yExtent[0] - Math.abs(yExtent[0]),
-      yExtent[0] + Math.abs(yExtent[0])
-    ];
-  }
-
-  yExtent = [nodeScenarioData.min, nodeScenarioData.max];
 
   const formatter = chartValueFormatter(...yExtent);
-  const xscale = d3
+  const historyChartXScale = d3
     .scaleLinear()
-    .domain(xExtent)
-    .range([0, width]);
-  const yscale = d3
+    .domain([historyStart, historyEnd])
+    .range([0, width * HISTORY_WIDTH_PERCENTAGE]);
+  const historyChartYScale = d3
     .scaleLinear()
     .domain(yExtent)
     .range([height, 0])
@@ -117,13 +86,11 @@ function render(
   svgGroup
     .append('rect')
     .classed('historical-rect', true)
+    .attr('x', historyChartXScale(historyStart))
     .attr('y', 0)
-    .attr('x', xscale(historyRange.start))
     .attr('height', height)
-    .attr('width', xscale(historyRange.end))
-    .attr('stroke', 'none')
-    .attr('fill', HISTORY_BACKGROUND_COLOR)
-    .attr('fill-opacity', 0.6);
+    .attr('width', historyChartXScale(historyEnd))
+    .attr('fill', HISTORY_BACKGROUND_COLOR);
 
   const historicG = svgGroup.append('g');
   historicG
@@ -134,8 +101,8 @@ function render(
       // FIXME: TypeScript shenanigans are necessary because timeseriesLine()
       //  is in a JS file. See svg-util.js for more details.
       timeseriesLine(
-        xscale,
-        yscale
+        historyChartXScale,
+        historyChartYScale
       )((filteredTimeSeries as any) as [number, number][]) as string
     )
     .style('stroke', HISTORY_LINE_COLOR)
@@ -145,28 +112,34 @@ function render(
 
   renderScenarioProjections(
     svgGroup,
-    xscale,
-    yscale,
+    width * HISTORY_WIDTH_PERCENTAGE,
+    width * (1 - HISTORY_WIDTH_PERCENTAGE),
+    height,
     nodeScenarioData,
     runOptions
   );
 
   // Axes
-  const yaxis = d3
-    .axisRight(yscale)
-    .tickValues(calculateGenericTicks(yscale.domain()[0], yscale.domain()[1]))
+  const yAxis = d3
+    .axisRight(historyChartYScale)
+    .tickValues(
+      calculateGenericTicks(
+        historyChartYScale.domain()[0],
+        historyChartYScale.domain()[1]
+      )
+    )
     .tickSize(0)
     // The type of formatter can't be more specific than `any`
     //  because under the hood d3.tickFormat requires d3.NumberType.
     // It correctly converts, but its TypeScript definitions don't
     //  seem to reflect that.
     .tickFormat(formatter as any);
-
+  // Render Y axis and hide domain line
   const yAxisElement = svgGroup
     .append('g')
     .classed('yaxis', true)
     .style('pointer-events', 'none')
-    .call(yaxis)
+    .call(yAxis)
     .style('font-size', '5px')
     .style('color', HISTORY_LINE_COLOR);
   yAxisElement.select('.domain').attr('stroke-width', 0);
@@ -175,8 +148,9 @@ function render(
 
 function renderScenarioProjections(
   svgGroup: D3GElementSelection,
-  xScale: D3ScaleLinear,
-  yScale: D3ScaleLinear,
+  xOffset: number,
+  width: number,
+  height: number,
   nodeScenarioData: NodeScenarioData,
   runOptions: { selectedScenarioId: string }
 ) {
@@ -216,22 +190,14 @@ function renderScenarioProjections(
   // Render one histogram for each selected time slice
   // FIXME: only render some slices depending on which ones are selected
   // FIXME: we should exit much earlier if no slices are selected
-  const availableWidth =
-    xScale.range()[1] - xScale(nodeScenarioData.projection_start);
-  const widthPerHistogram = availableWidth / histograms.length;
-  const availableHeight = Math.abs(yScale.range()[1] - yScale.range()[0]);
-  const heightPerHistogramBar = availableHeight / histograms[0].length;
+  const widthPerHistogram = width / histograms.length;
+  const heightPerHistogramBar = height / histograms[0].length;
   const histogramElements = svgGroup
     .selectAll('.histogram')
     .data(histograms)
     .join('g')
     .classed('histogram', true)
-    .attr('transform', (d, i) =>
-      translate(
-        xScale(nodeScenarioData.projection_start) + i * widthPerHistogram,
-        0
-      )
-    );
+    .attr('transform', (d, i) => translate(xOffset + i * widthPerHistogram, 0));
   const histogramBarElements = histogramElements
     .selectAll('.histogram-bar')
     .data(d => d)
