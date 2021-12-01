0f5223c14d45651dcd374382072fe1a7b68289a1
Maciej Kozlowski <mkozlowski@uncharted.software>
2021-12-01 11:05:04 -0500

    Add endpoint for fetching a spanning bounding box for multiple countries
    Rename gdm-name route -> gadm

--- server/src/routes/gadm.js
+++ server/src/routes/gadm.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+const _ = require('lodash');
 const router = express.Router();
 const { RESOURCE } = rootRequire('adapters/es/adapter');
-const { searchAndHighlight, queryStringBuilder } = rootRequire('adapters/es/client');
+const { client, searchAndHighlight, queryStringBuilder } = rootRequire('adapters/es/client');
 
 /**
  * GET Search fields based on partial matches
@@ -26,6 +27,52 @@ router.get('/suggestions', asyncHandler(async (req, res) => {
   const results = await searchAndHighlight(
     RESOURCE.GADM_NAME, builder.build(), filters, ['country', 'admin1', 'admin2', 'admin3']);
   res.json(results.map(result => result._source));
+}));
+
+/**
+ * GET A bounding box spanning all specified regions
+ **/
+router.get('/spanning-bbox', asyncHandler(async (req, res) => {
+  const regionIds = req.query.ids;
+
+  const termsQuery = {
+    bool: {
+      must: [
+        {
+          terms: {
+            'country.raw': regionIds
+          }
+        },
+        {
+          term: {
+            level: 'country'
+          }
+        }
+      ]
+    }
+  };
+
+  const aggregation = {
+    viewport: {
+      geo_bounds: {
+        field: 'bbox'
+      }
+    }
+  };
+
+  const query = {
+    size: 0,
+    query: termsQuery,
+    aggs: aggregation
+  };
+
+  const resp = await client.search({
+    index: RESOURCE.GADM_NAME,
+    body: query
+  });
+
+  const result = _.get(resp.body, 'aggregations.viewport.bounds', {});
+  res.json(result);
 }));
 
 module.exports = router;
