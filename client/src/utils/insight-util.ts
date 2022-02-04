e6372ec0b5879cae96727bf49a803c6039f22ccb
karlolsonuc <51097558+karlolsonuc@users.noreply.github.com>
2022-02-04 09:57:39 -0800

    Revert "#284 remove fix me comments"
    This reverts commit 1ab16b2c6f6bad46603982837297a5335e90b815.

--- client/src/utils/insight-util.ts
+++ client/src/utils/insight-util.ts
@@ -243,7 +243,7 @@ function generateInsightDOCX (
   const docxMaxImageSize = 612;
   const datacubeId = _.first(insight.context_id);
   const imageSize = scaleImage(insight.thumbnail, docxMaxImageSize, docxMaxImageSize);
-  const insightDate = dateFormatter(insight.modified_at);
+  const insightDate = dateFormatter(insight.modified_at); // FIXME: add modified_at field to type
   const footers = generateFooterDOCX(metadataSummary);
   const children = [
     new Paragraph({
@@ -377,7 +377,8 @@ function generateInsightPPTX (
 
   const datacubeId = _.first(insight.context_id);
   const imageSize = scaleImage(insight.thumbnail, widthLimitImage, heightLimitImage);
-  const insightDate = dateFormatter(insight.modified_at);
+  const insightDate = dateFormatter(insight.modified_at); // FIXME
+  const slide = pres.addSlide();
   const notes = `Title: ${insight.name}\nDescription: ${insight.description}\nCaptured on: ${insightDate}\n${metadataSummary}`;
 
   /*
