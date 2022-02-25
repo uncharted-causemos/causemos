c715b5a2a9eb32819cbb196d5f1133392bd2940d
Jaehwan Ryu <jryu@uncharted.software>
2022-02-25 10:43:52 -0500

    Changed if condition

--- client/src/services/geo-service.ts
+++ client/src/services/geo-service.ts
@@ -7,6 +7,6 @@ import API from '@/api/api';
 export const getBboxFromRegionIds = async (regionIds: string[]) => {
   const { data } = await API.post('gadm/spanning-bbox', { region_ids: regionIds });
   const { top_left: topLeft, bottom_right: bottomRight } = data;
-  if (!topLeft && !bottomRight) return null;
+  if (!topLeft || !bottomRight) return null;
   return [[topLeft.lon, topLeft.lat], [bottomRight.lon, bottomRight.lat]];
 };
