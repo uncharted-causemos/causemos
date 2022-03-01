bf91c7851c2f604404f5a6e481b35dd04b4432e2
Ahmed <amostafa@uncharted.software>
2022-03-01 15:47:15 -0500

    fix merge issue

--- client/src/components/widgets/datacube-region-ranking-card.vue
+++ client/src/components/widgets/datacube-region-ranking-card.vue
@@ -545,7 +545,7 @@ export default defineComponent({
               bins.forEach((bin) => {
                 _.sortBy(bin, item => item.value).forEach(dataItem => {
                   const normalizedValue = normalize(dataItem.value, dataExtent[0], dataExtent[1]);
-                  const finalNormalizedValue = invertData.value ? (1 - normalizedValue) : normalizedValue;
+                  const finalNormalizedValue = isDataInverted.value ? (1 - normalizedValue) : normalizedValue;
                   const barValue = showNormalizedData.value ? (finalNormalizedValue * numberOfColorBins.value) : dataItem.value;
 
                   let barColor = 'skyblue';
