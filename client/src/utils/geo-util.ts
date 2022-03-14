e9d77ca85cf3fa6687f05c3123927708a2cc12d1
Jaehwan Ryu <jryu@uncharted.software>
2022-03-14 11:58:29 -0400

    Added missing file

---
+++ client/src/utils/geo-util.ts
@@ -0,0 +1,28 @@
+
+/**
+ * Calculated the bounding box of all input coordinates
+ * @param coords an array of [lng, lat] coordinates
+ * @returns bounding box in [[minX, maxY], [maxX, minY]]
+ */
+export function bbox(coords: [number, number][]) {
+  const result = [[Infinity, -Infinity], [-Infinity, Infinity]]; // [[minX, maxY],[maxX, minY]]
+  coords.forEach(([lng, lat]) => {
+    if (result[0][0] > lng) {
+      result[0][0] = lng;
+    }
+    if (result[0][1] < lat) {
+      result[0][1] = lat;
+    }
+    if (result[1][0] < lng) {
+      result[1][0] = lng;
+    }
+    if (result[1][1] > lat) {
+      result[1][1] = lat;
+    }
+  });
+  return result;
+}
+
+export default {
+  bbox
+};
