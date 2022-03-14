2f012f96df2621edd53eafe877525db568733d8c
Maciej Kozlowski <mkozlowski@uncharted.software>
2022-03-14 15:26:19 -0400

    Filter out datacubes with is_hidden:true by default

--- server/src/adapters/es/datacube-query-util.js
+++ server/src/adapters/es/datacube-query-util.js
@@ -9,12 +9,17 @@ class DatacubeQueryUtil extends QueryUtil {
 
   buildQuery (filters) {
     const clauses = _.get(filters, 'clauses') || [];
+    const enableClause = clauses.find(clause => clause && clause.field === 'enable');
     const filterClauses = this.levelFilter(clauses, FIELD_LEVELS.DATACUBE);
     const nestedConceptClauses = this.levelFilter(clauses, FIELD_LEVELS.CONCEPTS);
+    const enableFilters = this.buildEnableFilters(enableClause);
     const generalFilters = this.buildFilters(filterClauses);
     const nestedConceptFilters = this.buildNestedFilters(nestedConceptClauses, NESTED_FIELD_PATHS.CONCEPTS);
 
-    const allFilters = [generalFilters];
+    const allFilters = [
+      enableFilters,
+      generalFilters
+    ];
     if (nestedConceptFilters != null) allFilters.push(nestedConceptFilters);
     return {
       query: {
@@ -23,6 +28,45 @@ class DatacubeQueryUtil extends QueryUtil {
         }
       }
     };
+  }
+
+  /**
+   * query builder to filter at the global (index) level
+   *
+   * By default:
+   *   * only return docs with is_hidden: false
+   *
+   * @params clause{}  enable clause object
+   * */
+  buildEnableFilters(clause) {
+    const values = _.get(clause, 'values') || [];
+    const queries = [];
+    if (values.indexOf('hidden') < 0) {
+      queries.push({
+        bool: {
+          should: [
+            {
+              term: { is_hidden: false }
+            },
+            {
+              bool: {
+                must_not: {
+                  exists: {
+                    field: 'is_hidden'
+                  }
+                }
+              }
+            }
+          ]
+        }
+      });
+    }
+    const result = {
+      bool: {
+        filter: queries
+      }
+    };
+    return result;
   }
 }
 
