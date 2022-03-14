cc6322c01034cd9d32e5826578f6ade39f607f74
Jaehwan Ryu <jryu@uncharted.software>
2022-03-14 12:09:49 -0400

    [lng, lat] -> [x, y]

--- client/src/utils/geo-util.ts
+++ client/src/utils/geo-util.ts
@@ -1,23 +1,23 @@
 
 /**
- * Calculated the bounding box of all input coordinates
+ * Calcuyed the bounding box of all input coordinates
  * @param coords an array of [x, y] coordinates
  * @returns bounding box in [[minX, maxY], [maxX, minY]]
  */
 export function bbox(coords: [number, number][]) {
   const result = [[Infinity, -Infinity], [-Infinity, Infinity]]; // [[minX, maxY],[maxX, minY]]
-  coords.forEach(([lng, lat]) => {
-    if (result[0][0] > lng) {
-      result[0][0] = lng;
+  coords.forEach(([x, y]) => {
+    if (result[0][0] > x) {
+      result[0][0] = x;
     }
-    if (result[0][1] < lat) {
-      result[0][1] = lat;
+    if (result[0][1] < y) {
+      result[0][1] = y;
     }
-    if (result[1][0] < lng) {
-      result[1][0] = lng;
+    if (result[1][0] < x) {
+      result[1][0] = x;
     }
-    if (result[1][1] > lat) {
-      result[1][1] = lat;
+    if (result[1][1] > y) {
+      result[1][1] = y;
     }
   });
   return result;
