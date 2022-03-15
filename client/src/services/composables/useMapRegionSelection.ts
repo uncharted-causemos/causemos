5bd576422b99e53d9a62043d1fb03c706ac29620
Jaehwan Ryu <jryu@uncharted.software>
2022-03-15 19:10:05 -0400

    Refactoring

--- client/src/services/composables/useMapRegionSelection.ts
+++ client/src/services/composables/useMapRegionSelection.ts
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import { Ref, computed } from 'vue';
 import { AdminRegionSets } from '@/types/Datacubes';
 import { SOURCE_LAYERS } from '@/utils/map-util-new';
-import { REGION_ID_DELIMETER, adminLevelToString } from '@/utils/admin-level-util';
+import { isRegionSelected } from '@/utils/admin-level-util';
 
 export default function useMapRegionSelection(
   selectedLayerId: Ref<string>,
@@ -15,26 +15,9 @@ export default function useMapRegionSelection(
     const checks = [country.size === 0, admin1.size === 0, admin2.size === 0, admin3.size === 0];
     return checks.slice(0, level + 1).reduce((prev, cur) => prev && cur, true);
   });
-  const isRegionSelected = (regionId: string) => {
-    const regionIdTokens = regionId.split(REGION_ID_DELIMETER);
-    // Check if the regionId is included in the selection. Check with the parent selection if there's a parent region selection.
-    let checkParentLevel = true;
-    while (regionIdTokens.length > 0 && checkParentLevel) {
-      const rid = regionIdTokens.join(REGION_ID_DELIMETER);
-      const curLevelRegionSelection = (selectedRegions.value)[adminLevelToString(regionIdTokens.length - 1) as keyof AdminRegionSets];
-      if (curLevelRegionSelection.has(rid)) {
-        return true;
-      }
-      // If there's no selection in the current level, check the selection from parent level.
-      checkParentLevel = curLevelRegionSelection.size === 0;
-      // Drop the current admin level region name from the id resulting the parent region id.
-      regionIdTokens.pop();
-    }
-    return false;
-  };
 
   return {
     isRegionSelectionEmpty,
-    isRegionSelected
+    isRegionSelected: (regionId: string) => isRegionSelected(selectedRegions.value, regionId)
   };
 }
