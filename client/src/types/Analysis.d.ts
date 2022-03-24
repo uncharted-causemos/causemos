d1867f661d5c04bebe3b1496f082b658677a53f8
bcodrington <bcodrington@uncharted.software>
2022-03-24 17:40:05 -0400

    Document the IDs that an analysisItem has

--- client/src/types/Analysis.d.ts
+++ client/src/types/Analysis.d.ts
@@ -8,9 +8,23 @@ export interface AnalysisItemState {
 }
 
 export interface AnalysisItem extends AnalysisItemState {
-  itemId: string; // unique uuid that uniquely identify this item
-  id: string; // datacube-id, e.g., unique indicator id
-  datacubeId: string; // datacub-family-id, e.g., indicator family id
+  // Generally a UUID that uniquely identifies one instance of a datacube in the
+  //  analysis. However, under some circumstances this can be the the datacube's
+  //  `id` or `data_id`. In those cases a UUID is not required to uniquely
+  //  identify the instance.
+  itemId: string;
+
+  // Uniquely identifies a model or indicator's metadata document in ES.
+  // Cannot be used to fetch data directly.
+  id: string;
+
+  // When combined with an output feature, can be used to fetch timeseries data.
+  // Indicators in the same "collection"/"dataset" share a `dataId`, but differ
+  //  by feature.
+  // In DOJO, these are all grouped into one "indicator".
+  // FIXME: rename to dataId to match the rest of the app
+  datacubeId: string;
+
   name?: string;
   selected?: boolean;
 }
