d64bfc573c58daa36c4dc8ae463682f45207d074
bcodrington <bcodrington@uncharted.software>
2022-06-15 15:50:41 -0400

    Fix off-by-one when modifying historical data

--- client/src/utils/date-util.ts
+++ client/src/utils/date-util.ts
@@ -33,26 +33,6 @@ const subtractMonths = (month: number, year: number, monthsToSubtract: number) =
   return getTimestampMillis(newYear, newMonth);
 };
 
-export const roundToNearestMonth = (timestamp: number) => {
-  const year = getYearFromTimestamp(timestamp);
-  const thisMonthIndex = getMonthFromTimestamp(timestamp);
-  const nextMonthIndex = (thisMonthIndex + 1) % 12;
-  const thisMonthTimestamp = Date.UTC(
-    year,
-    thisMonthIndex
-  );
-  const nextMonthTimestamp = Date.UTC(
-    nextMonthIndex === 0 ? year + 1 : year,
-    nextMonthIndex
-  );
-  if (timestamp - thisMonthTimestamp > nextMonthTimestamp - timestamp) {
-    // Closer to next month then the first date of this month
-    return nextMonthTimestamp;
-  }
-  // Closer to the first date of this month
-  return thisMonthTimestamp;
-};
-
 /** Returns a unix timestamp in milliseconds */
 export const getTimestampMillis = (year: number, month: number) => {
   return new Date(Date.UTC(year, month)).getTime();
