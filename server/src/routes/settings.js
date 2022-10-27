ec8ba7256fba9093397460c2d32057c128237d79
Jaehwan Ryu <jryu@uncharted.software>
2022-10-27 10:04:17 -0400

    Added client-settings api

--- server/src/routes/settings.js
+++ server/src/routes/settings.js
@@ -4,10 +4,13 @@ const router = express.Router();
 const config = require('../config/yargs-wrapper');
 const schedules = _.isEmpty(config.schedules) ? [] : config.schedules.split(',');
 
+const CLIENT_VAR_PREFIX = 'CLIENT';
+
 /* GET server settings */
 router.get('/settings', function(req, res, next) {
   const env = process.env;
   res.json({
+    envMode: env.ENV_MODE,
     logLevel: config.logLevel,
     cacheSize: config.cacheSize,
     dojoSync: config.dojoSync,
@@ -20,9 +23,27 @@ router.get('/settings', function(req, res, next) {
     senseiURL: env.SENSEI_URL,
     indraURL: env.INDRA_CURATION_URL,
     dartURL: env.DART_DOCUMENT_RETRIEVAL_URL,
+    dojoURL: env.DOJO_URL,
     pipelineURL: env.WM_PIPELINE_URL,
+    pipelineTargetS3URL: env.S3_URL,
+    pipelineTargetS3IndicatorsBucket: env.S3_INDICATORS_BUCKET,
+    pipelineTargetS3ModelsBucket: env.S3_MODELS_BUCKET,
+    requestQueueURL: env.WM_QUEUE_SERVICE_URL,
     schedules
   });
+});
+
+
+/* GET client settings */
+router.get('/client-settings', function(req, res, next) {
+  const env = process.env;
+  const clientSettings = {};
+  Object.entries(env).filter(([key, value]) => {
+    if (key.startsWith(CLIENT_VAR_PREFIX)) {
+      clientSettings[key] = value;
+    }
+  });
+  res.json(clientSettings);
 });
 
 module.exports = router;
