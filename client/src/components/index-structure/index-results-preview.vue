7c1a30a4627a0eae6eb00e76d7ae53bfd966b0b6
bcodrington <bcodrington@uncharted.software>
2023-02-06 15:59:06 -0500

    Route to results page when see-results is clicked - disable button when tree has no datasets

--- client/src/components/index-structure/index-results-preview.vue
+++ client/src/components/index-structure/index-results-preview.vue
@@ -1,11 +1,38 @@
 <template>
   <div class="index-results-preview-container">
     <h4>Index results</h4>
-    <button class="btn" disabled>See results</button>
+    <button :disabled="!canViewResults" class="btn" @click="seeResults">See results</button>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue';
+import router from '@/router';
+import { useStore } from 'vuex';
+import { ProjectType } from '@/types/Enums';
+import { indexNodeTreeContainsDataset } from '@/utils/indextree-util';
+import useIndexTree from '@/services/composables/useIndexTree';
+
+const props = defineProps<{
+  analysisId: string;
+}>();
+
+const indexTree = useIndexTree();
+const canViewResults = computed(() => indexNodeTreeContainsDataset(indexTree.tree.value));
+
+const store = useStore();
+const project = computed(() => store.getters['app/project']);
+const seeResults = () => {
+  router.push({
+    name: 'indexResults',
+    params: {
+      project: project.value,
+      analysisId: props.analysisId,
+      projectType: ProjectType.Analysis,
+    },
+  });
+};
+</script>
 
 <style lang="scss" scoped>
 @import '~styles/uncharted-design-tokens';
