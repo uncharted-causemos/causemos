505dbacc90ed2810e7dfd6f8c758717627c61cf5
Jason Baker <jason.baker@gutherie.ca>
2023-03-16 16:26:38 -0400

    JATAWARE semantic search for paragraphs:
    - Ensure missing data is represented with some information for the user.
    - verify the parameters exist before querying the jataware endpoint.

--- client/src/services/paragraphs-service.js
+++ client/src/services/paragraphs-service.js
@@ -4,13 +4,17 @@ const SEARCH_PATH = 'dojo/paragraphs/search';
 const GET_DOC_PATH = 'dojo/documents';
 
 export const searchParagraphs = async (searchString) => {
-  const result = await API.get(SEARCH_PATH, { params: { searchString } });
-  return result.data;
+  if (searchString) {
+    const result = await API.get(SEARCH_PATH, { params: { searchString } });
+    return result.data;
+  }
 };
 
 export const getDocument = async (docId) => {
-  const result = await API.get(`${GET_DOC_PATH}/${docId}`);
-  return result.data;
+  if (docId) {
+    const result = await API.get(`${GET_DOC_PATH}/${docId}`);
+    return result.data;
+  }
 };
 
 export default {
