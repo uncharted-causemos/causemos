b7b64007abdabe0f1c29828ff51195173ab1f575
bcodrington <bcodrington@uncharted.software>
2023-05-23 14:57:29 -0400

    Add tooltips and improve dataset weights interface

--- client/src/components/index-results/index-results-dataset-weights.vue
+++ client/src/components/index-results/index-results-dataset-weights.vue
@@ -5,17 +5,21 @@
       <p class="de-emphasized">with respect to {{ props.selectedNodeName }}</p>
     </header>
     <div class="list-items">
-      <div v-for="item of listItems" :key="item.dataset.id" class="list-item">
+      <div v-for="item of listItems" :key="item.conceptWithDataset.id" class="list-item">
         <i class="fa fa-fw" :style="{ color: DATASET_COLOR }" :class="[DATASET_ICON]" />
-        <span class="name">{{ item.dataset.name }}</span>
-        <i
-          v-if="item.oppositeEdgeCount > 0"
-          v-tooltip.top="item.dataset.name + ' represents low ' + tree.name + ' values.'"
-          class="fa fa-fw fa-arrow-right"
-          :style="{ color: NEGATIVE_COLOR }"
+        <span class="name">{{ item.conceptWithDataset.name }}</span>
+        <InvertedDatasetLabel
+          v-if="item.conceptWithDataset.dataset.isInverted"
+          v-tooltip.top="getInvertedTooltip(item)"
         />
-        <span v-if="item.oppositeEdgeCount > 1">{{ item.oppositeEdgeCount }}</span>
-        <InvertedDatasetLabel v-if="item.dataset.dataset.isInverted" />
+        <span
+          v-if="item.oppositeEdgeCount > 0"
+          v-tooltip.top="getOppositeEdgeTooltip(item)"
+          class="opposite-edges"
+        >
+          <i class="fa fa-fw fa-arrow-right" :style="{ color: NEGATIVE_COLOR }" />
+          <span v-if="item.oppositeEdgeCount > 1">{{ item.oppositeEdgeCount }}</span>
+        </span>
         <IndexResultsDatasetWeight class="dataset-weight" :weight="item.overallWeight" />
       </div>
     </div>
@@ -36,18 +40,41 @@ import { computed } from 'vue';
 import IndexResultsDatasetWeight from './index-results-dataset-weight.vue';
 import InvertedDatasetLabel from '../widgets/inverted-dataset-label.vue';
 import { NEGATIVE_COLOR } from '@/utils/colors-util';
+import { ConceptNodeWithDatasetAttached } from '@/types/Index';
 
 const props = defineProps<{ selectedNodeName: string }>();
 
 const { tree } = useIndexTree();
 
+interface ListItem {
+  conceptWithDataset: ConceptNodeWithDatasetAttached;
+  oppositeEdgeCount: number;
+  overallWeight: number;
+}
+
+const getInvertedTooltip = (item: ListItem) => {
+  return `High ${item.conceptWithDataset.dataset.datasetName} values represent low ${item.conceptWithDataset.name} values.`;
+};
+
+const getOppositeEdgeTooltip = (item: ListItem) => {
+  const count = item.oppositeEdgeCount;
+  const isOppositeEdgeCountOdd = count % 2 === 1;
+  return `There ${count === 1 ? 'is' : 'are'} ${count} opposite-polarity edge${
+    count === 1 ? '' : 's'
+  } between ${item.conceptWithDataset.name} and ${tree.value.name}, so ${
+    item.conceptWithDataset.name
+  } represents ${isOppositeEdgeCountOdd ? 'low' : 'high'} ${tree.value.name} values.`;
+};
+
 const listItems = computed(() => {
-  const datasets = findAllDatasets(tree.value);
-  const unsortedResults = datasets.map((dataset) => ({
-    dataset,
-    oppositeEdgeCount: countOppositeEdgesBetweenNodes(dataset, tree.value),
-    overallWeight: calculateOverallWeight(tree.value, dataset),
-  }));
+  const conceptsWithDatasets = findAllDatasets(tree.value);
+  const unsortedResults = conceptsWithDatasets.map(
+    (conceptWithDataset): ListItem => ({
+      conceptWithDataset,
+      oppositeEdgeCount: countOppositeEdgesBetweenNodes(conceptWithDataset, tree.value),
+      overallWeight: calculateOverallWeight(tree.value, conceptWithDataset),
+    })
+  );
   // Sort descending, by weight
   return _.sortBy(unsortedResults, (item) => -item.overallWeight);
 });
@@ -84,5 +111,9 @@ const listItems = computed(() => {
   // Nudge dataset weight upward to override text line-height and align weight with "inverted" label
   position: relative;
   bottom: 2px;
+}
+
+.opposite-edges {
+  margin-right: 5px;
 }
 </style>
