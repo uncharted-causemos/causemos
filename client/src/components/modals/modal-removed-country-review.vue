4fb3669392362a6457a15e66cc4694d87c3ff820
Jason Baker <jason.baker@gutherie.ca>
2023-05-30 14:05:33 -0400

    Add feature to filter countries during results view.
    - filter countries that are not present in all datasets (avoid bad math).
    - indicate countries have been removed in the right side panel (bar graphs).
    - provide a modal to review the removed countries and why (list datasets).

---
+++ client/src/components/modals/modal-removed-country-review.vue
@@ -0,0 +1,121 @@
+<template>
+  <modal @close="close()">
+    <template #header>
+      <h4>Hidden countries</h4>
+    </template>
+    <template #body>
+      <div class="container">
+        <nav>
+          <div class="country-col">Country</div>
+          <div class="missing-from">Not covered by</div>
+        </nav>
+        <div class="rows">
+          <div class="row" v-for="(country, index) in removedCountries" :key="index">
+            <div class="country-name">{{ country.countryName }}</div>
+            <div class="removed-from">
+              <div v-for="(dataSource, index2) in country.removedFrom" :key="index2">
+                {{ dataSource }}
+              </div>
+              <div v-if="country.removedFrom.length === 0">Not specified</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
+    <template #footer>
+      <ul class="unstyled-list">
+        <button ref="close" type="button" class="btn first-button" @click.stop="close()">
+          Close
+        </button>
+      </ul>
+    </template>
+  </modal>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Modal from '@/components/modals/modal.vue';
+
+export default defineComponent({
+  name: 'Removed Country Review',
+  components: {
+    Modal,
+  },
+  props: {
+    removedCountries: Array,
+  },
+  emits: ['close'],
+  mounted() {
+    const el = this.$refs.close as HTMLButtonElement;
+    el.focus();
+    console.log(JSON.stringify(this.removedCountries));
+  },
+  methods: {
+    close() {
+      this.$emit('close', null);
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+@import '@/styles/uncharted-design-tokens';
+
+:deep(.modal-container) {
+  width: max-content;
+  max-width: 80vw;
+  .modal-body {
+    height: 400px;
+    overflow-y: scroll;
+  }
+}
+
+.first-button {
+  margin-right: 10px;
+}
+
+.container {
+  width: 70vh;
+
+  nav {
+    color: $un-color-black-40;
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    align-items: center;
+    justify-content: center;
+    border-bottom: 1px solid $un-color-black-40;
+
+    .country-col {
+      width: 40%;
+    }
+
+    .missing-from {
+      width: 60%;
+    }
+    div.removed-row {
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+      div {
+        color: red;
+        width: 50%;
+      }
+    }
+  }
+  .rows {
+    .row {
+      margin-bottom: 8px;
+      display: flex;
+      flex-direction: row;
+      gap: 10px;
+      .country-name {
+        width: 40%;
+      }
+      .removed-from {
+        width: 60%;
+      }
+    }
+  }
+}
+</style>
