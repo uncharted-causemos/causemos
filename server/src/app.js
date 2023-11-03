8039f86d2d741eed3536bc547239b10d142a3451
bcodrington <bcodrington@uncharted.software>
2023-11-03 16:11:23 -0400

    Don't need authentication for static media assets

--- server/src/app.js
+++ server/src/app.js
@@ -112,6 +112,9 @@ app.use(
 
       // These end points are called by wm-queue without the keycloak auth token
       /\/api\/maas\/pipeline-reporting\/.+/,
+
+      // Static app media assets, e.g. /app/src/assets/thumbnail-dataset.png
+      /\/app\/src\/assets\/.+/,
     ],
   })
 );
