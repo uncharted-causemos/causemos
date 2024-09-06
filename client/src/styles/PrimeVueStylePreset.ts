1597fcbe3529e37a08aa81f2242d309c4bf5908c
bcodrington <bcodrington@uncharted.software>
2024-09-06 14:29:59 -0400

    feat: add primevue

---
+++ client/src/styles/PrimeVueStylePreset.ts
@@ -0,0 +1,20 @@
+import { definePreset } from '@primevue/themes';
+import Aura from '@primevue/themes/aura';
+
+export default definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '#FAF5FF',
+      100: '#F3E8FE',
+      200: '#E9D5FD',
+      300: '#CDB4FB',
+      400: '#BA97F7',
+      500: '#8858E9',
+      600: '#7740D6',
+      700: '#5D3AAE',
+      800: '#514398',
+      900: '#3A306E',
+      950: '#292556',
+    },
+  },
+});
