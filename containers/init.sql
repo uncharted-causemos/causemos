46ffde80bf8836be35e1fadbe16789b9bb00fdf3
David Gauldie <dgauldie@gmail.com>
2026-02-25 16:00:10 -0500

    remove keycloak theme since this is now hosted by IT

--- containers/init.sql
+++ containers/init.sql
@@ -4,6 +4,3 @@ ALTER USER keycloak WITH PASSWORD 'keycloak';
 CREATE DATABASE keycloak;
 GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
 ALTER DATABASE keycloak OWNER TO keycloak;
-
--- create Causemos DB
-CREATE DATABASE causemos;
